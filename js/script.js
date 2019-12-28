e632115fa082fe842241c6d5b05d63f5588e8544
Khamoido Ervin Vitalievich <hamoydoerik@gmail.com>
2019-12-28 10:22:04 +0300

    Add .jshintrc file

---
+++ js/script.js
@@ -0,0 +1,23 @@
+'use strict'; 
+
+let money = prompt("Каков Ваш бюджет на месяц?", ""),
+    time = prompt("Введите дату в формате YYYY-MM-DD", "");
+    
+let appData = {
+  buget: money,
+  timeData: time,
+  expenses: {},
+  optionalExpenses: {},
+  income: [],
+  savings: false
+};
+
+let answer1 = prompt("Введите обязательную статью расходив в этом месяце", ""),
+    answer2 = prompt("Во сколько обойдётся?", ""),
+    answer3 = prompt("Введите обязательную статью расходив в этом месяце", ""),
+    answer4 = prompt("Во сколько обойдётся?", "");
+
+appData.expenses[answer1] = answer2; //вместо скобок можно поставить точку 
+appData.expenses[answer3] = answer4; //вместо скобок можно поставить точку
+
+alert(appData.budget / 30);
