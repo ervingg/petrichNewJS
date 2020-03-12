ed37148dfce63f3b1697e83b30603dce7deac2bb
Ervin <hamoydoerik@gmail.com>
2020-03-12 18:21:45 +0200

    Add 10th misson

--- js/yoga_project/js/script.js
+++ js/yoga_project/js/script.js
@@ -34,4 +34,58 @@ window.addEventListener('DOMContentLoaded', function() {
       }
     }
   });
+
+  // TIMER
+  let deadline = '2020-03-13';
+
+  function getTimeRemaining(endtime) {
+    let t = Date.parse(endtime) - Date.parse(new Date()),
+        seconds = Math.floor( (t / 1000) % 60),
+        minutes = Math.floor( (t / 1000 / 60) % 60),
+        hours = Math.floor( (t / (1000 * 60 * 60) ) );
+        // Хвостик с часами
+        // hours = Math.floor( (t / 1000 / 60 / 60) % 24),
+        // Дни 
+        // days = Math.floor( (t / (1000 * 60 * 60 * 24) ) );
+
+        return {
+          'total' : t,
+          'hours' : hours,
+          'minutes' : minutes,
+          'seconds' : seconds
+        };
+  }
+
+  function setClock(id, endtime) {
+    let timer = document.getElementById(id),
+      hours = timer.querySelector('.hours'),
+      minutes = timer.querySelector('.minutes'),
+      seconds = timer.querySelector('.seconds'),
+      timeInterval = setInterval(updateClock, 1000);
+
+    function updateClock() {
+      let t = getTimeRemaining(endtime);
+
+      function addZiro(num) {
+        if (num <= 9) {
+          return '0' + num;
+        } else {
+          return num;
+        }
+      }
+
+      hours.textContent = addZiro(t.hours);
+      minutes.textContent = addZiro(t.minutes);
+      seconds.textContent = addZiro(t.seconds);
+
+      if (t.total <= 0) {
+        clearInterval(timeInterval);
+        hours.textContent = '00';
+        minutes.textContent = '00';
+        seconds.textContent = '00';
+      }
+    }
+  }
+
+  setClock('timer', deadline);
 });
