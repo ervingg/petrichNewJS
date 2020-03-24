f6d59cd2135f21e7bbe2cc5f3ef500482f4e2b0a
Ervin <hamoydoerik@gmail.com>
2020-03-24 18:41:20 +0200

    Поправочка

--- js/mission_11_part_2/script.js
+++ js/mission_11_part_2/script.js
@@ -3,6 +3,6 @@
 let age = document.getElementById('age');
 
 function showUser(surname, name) {
-	console.log("Пользователь " + surname + " " + name + ", его возраст " + this.value);
+	console.log(`Пользователь ${surname}  ${name}, его возраст ${this.value}`);
 }
 showUser.call(age, 'Jonh', 'Smith');
