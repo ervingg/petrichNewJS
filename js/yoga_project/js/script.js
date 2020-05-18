5867a7b1b74708475fb04fb2d55432b5e4ff7cce
Ervin <hamoydoerik@gmail.com>
2020-05-18 17:44:35 +0300

    Add mission 16

--- js/yoga_project/js/script.js
+++ js/yoga_project/js/script.js
@@ -202,56 +202,146 @@ window.addEventListener('DOMContentLoaded', function () {
 	sendForm(formFooter);
 
 	//SLIDER
-	let slideIndex = 1, //Тот слайд, который показывается в текущий момент
+	// let slideIndex = 1, //Тот слайд, который показывается в текущий момент
+	// 	slides = document.querySelectorAll('.slider-item'),
+	// 	prev = document.querySelector('.prev'),
+	// 	next = document.querySelector('.next'),
+	// 	dotsWrap = document.querySelector('.slider-dots'),
+	// 	dots = document.querySelectorAll('.dot');
+
+	// showSlides(slideIndex);
+
+	// function showSlides(n) {
+	// 	if (n > slides.length) {
+	// 		slideIndex = 1;
+	// 	}
+
+	// 	if (n < 1) {
+	// 		slideIndex = slides.length;
+	// 	}
+
+	// 	slides.forEach((item) => item.style.display = 'none');
+	// 	// for (let i = 0; i < slides.length; i++) { 					//ТОЖЕ ЧТО И ВЫШЕ
+	// 	// 	slides[i].style.display = 'none';
+	// 	// }
+
+	// 	dots.forEach((item) => item.classList.remove('dot-active'));
+
+	// 	slides[slideIndex - 1].style.display = 'block';
+	// 	dots[slideIndex - 1].classList.add('dot-active');
+	// }
+
+	// function plusSlides(n) {
+	// 	showSlides(slideIndex += n);
+	// }
+
+	// function currentSlide(n) { //ОПРЕДЕЛЯЕТ ТЕКУЩИЙ СЛАЙД
+	// 	showSlides(slideIndex = n); //И УСТАНАВЛИВАЕТ ЕГО
+	// }
+
+	// prev.addEventListener('click', function () {
+	// 	plusSlides(-1);
+	// });
+
+	// next.addEventListener('click', function () {
+	// 	plusSlides(1);
+	// });
+
+	// dotsWrap.addEventListener('click', function (event) {
+	// 	for (let i = 0; i < dots.length + 1; i++) {
+	// 		if (event.target.classList.contains('dot') && event.target == dots[i - 1]) {
+	// 			currentSlide(i);
+	// 		}
+	// 	}
+	// });
+
+	let sliderIndex = 1,
 		slides = document.querySelectorAll('.slider-item'),
 		prev = document.querySelector('.prev'),
 		next = document.querySelector('.next'),
-		dotsWrap = document.querySelector('.slider-dots'),
+		wrap = document.querySelector('.slider-dots'),
 		dots = document.querySelectorAll('.dot');
 
-	showSlides(slideIndex);
+	showSlides(sliderIndex);
 
 	function showSlides(n) {
 		if (n > slides.length) {
-			slideIndex = 1;
+			sliderIndex = 1;
 		}
 
 		if (n < 1) {
-			slideIndex = slides.length;
+			sliderIndex = slides.length;
 		}
 
 		slides.forEach((item) => item.style.display = 'none');
-		// for (let i = 0; i < slides.length; i++) { 					//ТОЖЕ ЧТО И ВЫШЕ
-		// 	slides[i].style.display = 'none';
-		// }
-
 		dots.forEach((item) => item.classList.remove('dot-active'));
 
-		slides[slideIndex - 1].style.display = 'block';
-		dots[slideIndex - 1].classList.add('dot-active');
+		slides[sliderIndex - 1].style.display = 'block';
+		dots[sliderIndex - 1].classList.add('dot-active');
 	}
 
-	function plusSlides(n) {
-		showSlides(slideIndex += n);
+	function plusSlide(n) {
+		showSlides(sliderIndex += n); //!!!!!!!!!!!!!!!!!!
 	}
 
-	function currentSlide(n) { //ОПРЕДЕЛЯЕТ ТЕКУЩИЙ СЛАЙД
-		showSlides(slideIndex = n); //И УСТАНАВЛИВАЕТ ЕГО
+	function currentSlide(n) {
+		showSlides(sliderIndex = n);
 	}
 
 	prev.addEventListener('click', function () {
-		plusSlides(-1);
+		plusSlide(-1);
 	});
 
 	next.addEventListener('click', function () {
-		plusSlides(1);
+		plusSlide(1);
 	});
 
-	dotsWrap.addEventListener('click', function (event) {
-		for (let i = 0; i < dots.length + 1; i++) {
+	wrap.addEventListener('click', function (event) {
+		for (let i = 0; i <= dots.length; i++) {
 			if (event.target.classList.contains('dot') && event.target == dots[i - 1]) {
 				currentSlide(i);
 			}
 		}
 	});
+
+	//CALC
+	let persons = document.querySelectorAll('.counter-block-input')[0],
+		restDays = document.querySelectorAll('.counter-block-input')[1],
+		place = document.getElementById('select'),
+		totalValue = document.getElementById('total'),
+		personsSum = 0,
+		daysSum = 0,
+		total = 0;
+
+	persons.addEventListener('input', function () {
+		personsSum = +this.value;
+		total = (daysSum + personsSum) * 4000;
+
+		if (restDays.value == '' || (daysSum + personsSum) * 4000 <= 4000) {
+			totalValue.innerHTML = 0;
+		} else {
+			totalValue.innerHTML = total;
+		}
+	});
+
+	restDays.addEventListener('input', function () {
+		daysSum = +this.value;
+		total = (daysSum + personsSum) * 4000;
+
+		if (persons.value == '' || (daysSum + personsSum) * 4000 <= 4000) {
+			totalValue.innerHTML = 0;
+		} else {
+			totalValue.innerHTML = total;
+		}
+	});
+
+	place.addEventListener('change', function () {
+		if (restDays.value == '' || persons.value == '') {
+			totalValue.innerHTML = 0;
+		} else {
+			let a = total;
+			totalValue.innerHTML = a * this.options[this.selectedIndex].value;
+		}
+	});
+
 });
