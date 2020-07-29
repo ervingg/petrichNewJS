37110d6df7291336ed3e80b1fb451992ce8403df
Ervin <hamoydoerik@gmail.com>
2020-07-29 14:45:39 +0300

    React_first_project is added

---
+++ js/react_first_project/src/components/app-header/app-header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './app-header.css';
+
+const AppHeader = () => {
+  return (
+    <div className="app-header d-flex">
+      <h1>Ervin Khamoido</h1>
+      <h2>5 posts, like 0</h2>
+    </div>
+  )
+}
+
+export default AppHeader;	
