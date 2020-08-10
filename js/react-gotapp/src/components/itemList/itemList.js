819f63ef73d0dbde88ec7ac1d86fe71b8c4f9cb6
Ervin <hamoydoerik@gmail.com>
2020-08-10 13:26:20 +0300

    Fixed id problem, added error_message

--- js/react-gotapp/src/components/itemList/itemList.js
+++ js/react-gotapp/src/components/itemList/itemList.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-// import './itemList.css';
 import gotService from '../../services/gotService';
 import styled from 'styled-components';
 import Spinner from '../spinner';
+import ErrorMessage from '../errorMessage';
 
 const ListGroupItem = styled.li`
     cursor: pointer;
@@ -13,27 +13,45 @@ export default class ItemList extends Component {
     gotService = new gotService();
 
     state = {
-        charList: null
+        charList: null,
+        error: false
     }
 
     componentDidMount() {
         this.gotService.getAllCharacters()
             .then( (charList) => {
                 this.setState({
-                    charList
-                })
+                    charList,
+                    error: false
+                });
             })
+            .catch(() => {this.onError()});
+    }
+
+    componentDidCatch() {
+        this.setState({
+            charList: null,
+            error: true
+        })
+    }
+
+    onError(status){
+        this.setState({
+            charList: null,
+            error: true
+        })
     }
 
     //renderItems принимает массив потому что из API нам приходит array
     renderItems(arr) {
-        return arr.map((item, i) => {
+        return arr.map((item) => {
+            const {id, name} = item;
             return (
                 <ListGroupItem 
-                    key={i}
+                    key={id}
                     className="list-group-item"
-                    onClick={() => this.props.onCharSelected(41 + i)}>
-                    {item.name}
+                    onClick={() => this.props.onCharSelected(id)}>
+                    {name}
                 </ListGroupItem>
             )
         })
@@ -41,7 +59,11 @@ export default class ItemList extends Component {
     
     render() {
 
-        const {charList} = this.state;
+        const {charList, error} = this.state;
+
+        if (error){
+            return <ErrorMessage/>
+        }
 
         if (!charList) {
             return <Spinner/>
