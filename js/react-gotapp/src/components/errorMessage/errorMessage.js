51c9cfcb6e021bc5ac676a389d055bce222795b2
Ervin <hamoydoerik@gmail.com>
2020-08-10 13:17:19 +0300

    Code refactoring

--- js/react-gotapp/src/components/errorMessage/errorMessage.js
+++ js/react-gotapp/src/components/errorMessage/errorMessage.js
@@ -4,11 +4,11 @@ import img from './error.jpg';
 
 const ErrorMessage = () => {
   return (
-    <>
+    <div className="error-wrapper">
       {/* <img src={process.env.PUBLIC_URL + '/img/error.jpg'} alt="error"/> */}
       <img src={img} alt="error"/>
       <span className="error-text">Something goes wrong</span>
-    </>
+    </div>
   )
 }
 
