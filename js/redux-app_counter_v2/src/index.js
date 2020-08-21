878df9fbcc5f5ab211cd80fd15ed3d76f5650efc
Ervin <hamoydoerik@gmail.com>
2020-08-21 21:45:28 +0300

    Code refactoring

--- js/redux-app_counter_v2/src/index.js
+++ js/redux-app_counter_v2/src/index.js
@@ -1,40 +1,14 @@
-import {
-	createStore
-} from 'redux';
-
-const reducer = (state = 0, action) => {
-	switch (action.type) {
-		case 'INC':
-			return state + 1;
-		case 'DEC':
-			return state - 1;
-		case 'REF':
-			return 0;
-		default:
-			return state;
-	}
-};
-
-const inc = () => ({type: 'INC'});
-const dec = () => ({type: 'DEC'});
-const rnd = () => ({type: 'REF'});
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import reducer from './reducer';
+import React from 'react'; 
+import ReactDOM from 'react-dom';
+import App from './components/app';
+import './style.css';
 
 const store = createStore(reducer);
 
-document.querySelector('#inc').addEventListener('click', () => {
-	store.dispatch(inc());
-});
-
-document.querySelector('#dec').addEventListener('click', () => {
-	store.dispatch(dec());
-});
-
-document.querySelector('#ref').addEventListener('click', () => {
-	store.dispatch(rnd());
-});
-
-const update = () => {
-	document.querySelector('.counter').textContent = store.getState();
-};
-
-store.subscribe(update);
+ReactDOM.render(
+    <Provider store={store}> 
+        <App/>
+    </Provider>, document.getElementById('root'));
