ff5e66b4012fe683d29cf877e702e661736da974
Ervin <hamoydoerik@gmail.com>
2020-09-08 21:21:05 +0300

    Created navigation with react-router-dom

--- js/resto/src/components/app/app.js
+++ js/resto/src/components/app/app.js
@@ -2,17 +2,23 @@ import React from 'react';
 import {MainPage, CartPage} from '../pages';
 import AppHeader from '../app-header';
 import WithRestoService from '../hoc';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 import Background from './food-bg.jpg';
 
 const App = ({RestoService}) => {
     console.log(RestoService.getMenuItems());
     return (
-        <div style={{background: `url(${Background}) center center/cover no-repeat`}} className="app">
-            <AppHeader total={50}/>
-            <MainPage/>
-            <CartPage/>
-        </div>
+        <Router>
+            <div style={{background: `url(${Background}) center center/cover no-repeat`}} className="app">
+                <AppHeader total={50}/>
+                
+                <Switch>
+                    <Route path="/" component={MainPage} exact />
+                    <Route path="/total" component={CartPage}/>
+                </Switch>
+            </div>
+        </Router>
     )
 }
 
